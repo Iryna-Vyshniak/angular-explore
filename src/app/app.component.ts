7048def725b03b6e6de9a3578d917db3371fc6ad
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-12-28 21:09:23 +0200

    feat: add two-way binding example in form
    - Implemented two-way data binding using [(ngModel)] for userName and userEmail
    - Created input field to dynamically update userName and display it in real-time
    - Added form with email input and submit button bound to userEmail
    - Disabled submit button until userEmail is provided
    - Created submitForm() method to log userEmail to the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './components/test/test.component';
+import { ImageComponent } from './components/image/image.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent, ImageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-test',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 
 export class TestComponent {
   title: string = 'ANGULAR EXPLORE';
-  firstName: string = 'Mr.'
-  secondName: string = 'Fletcher Sheldon'
+  firstName: string = 'Mr.';
+  secondName: string = 'Fletcher Sheldon';
   isActive: boolean = true;
   isDisabled: boolean = false;
   isClickState: boolean = false;
+  userName: string = '';
+  userEmail: string = '';
+
 
   getFullName() {
     return `${this.firstName} ${this.secondName}`
@@ -22,5 +26,9 @@ export class TestComponent {
 
   toggleState() {
     return this.isClickState = !this.isClickState;
+  }
+
+  submitForm() {
+    console.log('Email enter by user: ', this.userEmail);
   }
 }
